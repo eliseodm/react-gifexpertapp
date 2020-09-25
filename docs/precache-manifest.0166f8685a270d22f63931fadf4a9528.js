93f17f7e0e6bd1dd26987fd1aed37ce7ea13e6f4
Eliseo Diaz Medina <eliseodm@gmail.com>
2020-09-24 21:09:05 -0300

    Docs Agregado

---
+++ docs/precache-manifest.0166f8685a270d22f63931fadf4a9528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b6326158ca0188a96182e6ebfddeee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e89965d74497b3e196",
+    "url": "/static/css/main.001e148f.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e89965d74497b3e196",
+    "url": "/static/js/main.e75541a0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
